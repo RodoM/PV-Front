c3bdbac770d5e0a4bf25afb7fd3450019459d9d9
RodoM <rodomeroi@gmail.com>
2025-04-12 17:50:58 -0300

    add: eslint y prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
+import prettierPlugin from "eslint-plugin-prettier";
+import importPlugin from "eslint-plugin-import";
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx"],
+        },
+        alias: {
+          map: [["@", "./src"]],
+          extensions: [".js", ".jsx"],
+        },
+      },
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "prettier/prettier": "error",
+      "no-unused-vars": ["error", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      semi: ["error", "always"], // Asegura que siempre haya punto y coma al final de la línea
+      quotes: ["error", "double"], // Usa comillas dobles
+      "no-console": "warn", // Evita el uso de console.log
+      "comma-dangle": ["error", "always-multiline"], // Forzar las comas finales
+      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }], // Permite JSX en archivos .js
     },
   },
-]
+];
