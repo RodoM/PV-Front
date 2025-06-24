f09b5d38613e14d441c3413ca4c91c86e7943154
RodoM <rodomeroi@gmail.com>
2025-06-24 19:51:27 -0300

    fix: consultar todas las marcas, categorias y rubros

--- src/components/auth/steps/step-2.jsx
+++ src/components/auth/steps/step-2.jsx
@@ -118,7 +118,9 @@ const Step2 = forwardRef(({ defaultValues }, ref) => {
   }));
 
   useEffect(() => {
-    api.get("/sector/list").then((res) => setRubroOptions(res.data.data));
+    api
+      .get("/sector/list", { pageNumber: 1, pageSize: 1000 })
+      .then((res) => setRubroOptions(res.data.data));
   }, []);
 
   useEffect(() => {
